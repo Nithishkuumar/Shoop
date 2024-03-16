aafd2192c239383cbb05ad712977a2cfd1389f2d
Nithishkuumar <154660155+Nithishkuumar@users.noreply.github.com>
2024-03-16 23:11:53 +0530

    chane

--- backend/Server.js
+++ backend/Server.js
@@ -34,8 +34,8 @@ app.use("/uploads",express.static(path.join(_dirname,"/uploads")))
 
 //     // app.get("*",(req,res)=>res.sendFile(path.resolve(__dirname,"frontend","build","index.html")))
 // }else{
-//     app.get("/",(req,res)=>{
-//         res.send("API is running")
+    app.get("/",(req,res)=>{
+        res.send("API is running")
 //     })
 // }
 
