f0b768ab6b8663bbcdb2021e006781d529dd5bcc
Nithishkuumar <154660155+Nithishkuumar@users.noreply.github.com>
2024-03-16 23:37:07 +0530

    cges

--- backend/Server.js
+++ backend/Server.js
@@ -22,9 +22,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
-app.get('/',(req,res)=>{
-    res.send("API is running")
-})
+// app.get('/',(req,res)=>{
+//     res.send("API is running")
+// })
 
 const _dirname = path.resolve();
 app.use("/uploads",express.static(path.join(_dirname,"/uploads")))
@@ -38,6 +38,22 @@ app.use("/uploads",express.static(path.join(_dirname,"/uploads")))
 //         res.send("API is running")
 // //     })
 // }
+
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use('/uploads', express.static('/var/data/uploads'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+  } else {
+    const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  } 
 
 app.use('/api/products/',productrouter)
 app.use('/api/users/',userrouter)
