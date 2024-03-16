2950a7dc93965a4e3ecec9105bd8c6bbb1fcc682
Nithishkuumar <154660155+Nithishkuumar@users.noreply.github.com>
2024-03-16 23:17:10 +0530

    ch

--- backend/Server.js
+++ backend/Server.js
@@ -22,9 +22,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
-// app.get('/',(req,res)=>{
-//     res.send("API is running")
-// })
+app.get('/',(req,res)=>{
+    res.send("API is running")
+})
 
 const _dirname = path.resolve();
 app.use("/uploads",express.static(path.join(_dirname,"/uploads")))
@@ -34,9 +34,9 @@ app.use("/uploads",express.static(path.join(_dirname,"/uploads")))
 
 //     // app.get("*",(req,res)=>res.sendFile(path.resolve(__dirname,"frontend","build","index.html")))
 // }else{
-    app.get("/",(req,res)=>{
-        res.send("API is running")
-//     })
+//     app.get("/",(req,res)=>{
+//         res.send("API is running")
+// //     })
 // }
 
 app.use('/api/products/',productrouter)
